bbd7599c242c438626895a09a0bb5a4818e25dfc
brunoferreira182 <brunoferreira1821995@gmail.com>
2023-09-07 15:13:53 -0300

    ajuste organismdetail

--- src/pages/admin/OrganismDetail.vue
+++ src/pages/admin/OrganismDetail.vue
@@ -194,6 +194,18 @@
                       </q-item-section>
                     </q-item>
                   </q-expansion-item>
+                  <div 
+                    class="text-caption text-grey-7 q-mx-md"
+                    v-if="func.functionProperties && func.functionProperties.numRequired === true"
+                  >
+                    Esta função requer {{ func.functionNumOfOccupants }} participantes
+                  </div>
+                  <div 
+                    class="text-caption text-grey-7 q-mx-md"
+                    v-else
+                  >
+                    Esta função permite {{ func.functionNumOfOccupants }} participantes
+                  </div>
                   <q-item-section class="q-pa-xs">
                     <q-btn
                       label="Adicionar pessoa"
