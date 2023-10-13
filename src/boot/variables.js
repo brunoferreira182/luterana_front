4ccad5fda538262f0d7fe0c99bd9adb14aa1b425
Leonardo <leorocha2004@gmail.com>
2023-10-13 14:26:24 -0300

    unificar front

--- src/boot/variables.js
+++ src/boot/variables.js
@@ -1,5 +1,5 @@
 //////////// VARIAVEIS DA APLICACAO //////
-const MODE_MASTER_SERVER = "prod";
+const MODE_MASTER_SERVER = "tiago";
 const COMPANY_ID = 10;
 // NAO ALTERAR //
 const MODE_IFRAME = MODE_MASTER_SERVER;
