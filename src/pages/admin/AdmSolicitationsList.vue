a8e2c47d001bd8467ba612d86682bd3bc7f4a482
brunoferreira182 <brunoferreira1821995@gmail.com>
2023-10-19 17:24:07 -0300

    ajuste

--- src/pages/admin/AdmSolicitationsList.vue
+++ src/pages/admin/AdmSolicitationsList.vue
@@ -57,7 +57,7 @@
           <q-card-section class="q-gutter-md">
             <q-input
               outlined
-              label="Descrição"
+              label="Título"
               autogrow
               v-model="dialogNewSugestion.suggestionTitle"
             />
