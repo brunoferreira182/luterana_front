f03167c4a9c92b648971acbc0d4a719518f27297
tiago_razera <tiago.razera@pelservicos.com.br>
2023-10-24 18:28:16 -0300

    tirado timeout

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,31 @@
 </template>
 
 <script>
-import { useAutoLogoutStore } from "stores/autoLogout";
+// import { useAutoLogoutStore } from "stores/autoLogout";
 import { defineComponent } from 'vue'
-import { useRouter } from 'vue-router';
+// import { useRouter } from 'vue-router';
 export default defineComponent({
   name: 'App',
   created() {
-    const router = useRouter()
-    const autoLogoutStore = useAutoLogoutStore();
+    // const router = useRouter()
+    // const autoLogoutStore = useAutoLogoutStore();
     
-    autoLogoutStore.startAutoLogoutTimer();
+    // autoLogoutStore.startAutoLogoutTimer();
     
-    window.addEventListener('mousemove', () => {
-      autoLogoutStore.resetAutoLogoutTimer();
-    });
+    // window.addEventListener('mousemove', () => {
+    //   autoLogoutStore.resetAutoLogoutTimer();
+    // });
     
-    window.addEventListener('click', () => {
-      autoLogoutStore.resetAutoLogoutTimer();
-    });
+    // window.addEventListener('click', () => {
+    //   autoLogoutStore.resetAutoLogoutTimer();
+    // });
 
-    autoLogoutStore.$subscribe((state) => {
-      if (state.events.target.timer === null) {
-        localStorage.clear();
-        router.replace('/login')
-      }
-    });
+    // autoLogoutStore.$subscribe((state) => {
+    //   if (state.events.target.timer === null) {
+    //     localStorage.clear();
+    //     router.replace('/login')
+    //   }
+    // });
   },
 })
 
