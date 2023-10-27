ec449e68b91f1672dfc3d76e7881597ea7d3c088
Tiago Razera <tiago.razera@pelservicos.com.br>
2023-10-27 08:24:58 -0300

    tirando do master server ok

--- src/boot/masterServerRoutes.js
+++ src/boot/masterServerRoutes.js
@@ -1,13 +1,26 @@
-import { MODE_MASTER_SERVER } from './variables'
+import { MODE_MASTER_SERVER, MODE_SERVER } from './variables'
+
+const port = MODE_SERVER === 'master' ? 8000 : 8800
 
 function masterServerRoute () {
   let ret
-  if (MODE_MASTER_SERVER === 'local') ret = 'http://localhost:8000'
-  else if (MODE_MASTER_SERVER === 'darta') ret = 'http://192.168.1.3:8000/'
-  else if (MODE_MASTER_SERVER === 'tiago') ret = 'http://192.168.1.7:8000/'
-  else if (MODE_MASTER_SERVER === 'dev') ret = 'https://api6.b3dev.dev:8000/'
-  else if (MODE_MASTER_SERVER === 'prod') ret = 'https://api2.b3dev.dev:8000/'
+  if (MODE_MASTER_SERVER === 'local') ret = 'http://localhost:' + port
+  else if (MODE_MASTER_SERVER === 'darta') ret = 'http://192.168.1.3:' + port
+  else if (MODE_MASTER_SERVER === 'tiago') ret = 'http://192.168.1.7:' + port
+  else if (MODE_MASTER_SERVER === 'dev') ret = 'https://api6.b3dev.dev:' + port
+  else if (MODE_MASTER_SERVER === 'prod') ret = 'https://api2.b3dev.dev:' + port
   return ret
 }
 
-export { masterServerRoute }
+function calculateMasterServerAttachmentsRoute() {
+  const extraRoute = MODE_SERVER === 'master' ? '/static/escolas_app/' : '/static/'
+  if (
+    MODE_MASTER_SERVER === "dev" ||
+    MODE_MASTER_SERVER === "dev_ext" ||
+    MODE_MASTER_SERVER === "prod"
+  ) {
+    return masterServerRoute() + extraRoute
+  } else return masterServerRoute("dev_ext") + extraRoute
+}
+
+export { masterServerRoute, calculateMasterServerAttachmentsRoute }
