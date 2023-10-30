e570fff08dd2be7e3b405dcb89ab899dd7a187e7
Leonardo <leorocha2004@gmail.com>
2023-10-30 11:05:19 -0300

    inicio attach adm

--- src/pages/admin/AdmDocuments.vue
+++ src/pages/admin/AdmDocuments.vue
@@ -5,16 +5,15 @@
         flat
         class="bg-accent"
         title="Lista de documentos"
-        :columns="suggestionData"
-        :rows="suggestionList"
+        :columns="attachData"
+        :rows="attachList"
         row-key="_id"
         virtual-scroll
         rows-per-page-label="Registros por página"
         no-data-label="Nenhum dado inserido até o momento"
         no-results-label="A pesquisa não retornou nenhum resultado"
         :rows-per-page-options="[10, 20, 30, 50]"
-        @row-click="clkGoToFunctionInOrganismDetail"
-        :selected-rows-label="getSelectedString"
+        @row-click="clkGoToAttachDetail"
         v-model:pagination="pagination"
         @request="nextPage"
       >
@@ -97,19 +96,50 @@
           </q-card-actions>
         </q-card>
       </q-dialog>
+      <q-dialog v-model="dialogAttachDetail.open" @hide="clearDialogDetail()">
+        <q-card style="border-radius: 1rem; height: 150x; width: 400px">
+          <div class="text-h6 text-center q-pa-md ">{{ dialogAttachDetail.attachDetail.attachTitle }}</div>
+          <q-card-section class="q-gutter-md">
+            <q-input
+              outlined
+              hint="Descrição"
+              v-model="dialogAttachDetail.attachDetail.attachDescription"
+              readonly
+            />
+            <q-img 
+              style="border-radius: 1rem"
+              :src=" utils.makeFileUrl(dialogAttachDetail.attachDetail.attach.filename)" 
+              width="108px" 
+              height="108px"
+            />
+          </q-card-section>
+          <q-card-actions align="center">
+            <q-btn
+              flat
+              label="Voltar"
+              no-caps
+              color="primary"
+              rounded
+              @click="clearDialogDetail"
+            />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
     </q-page>
   </q-page-container>
 </template>
+<script setup>
+import utils from '../../boot/utils'
+</script>
 <script>
 import { defineComponent } from "vue";
 import useFetch from "../../boot/useFetch";
 import { useTableColumns } from "stores/tableColumns";
-
 export default defineComponent({
   name: "DocumentList",
   data() {
     return {
-      suggestionData: useTableColumns().suggestionList,
+      attachData: useTableColumns().admAttachList,
       attachList: [],
       pagination: {
         page: 1,
@@ -119,7 +149,11 @@ export default defineComponent({
         open: false,
         title: '',
         description: '',
-        files: []
+        files: null
+      },
+      dialogAttachDetail: {
+        open: false,
+        attachDetail: null
       },
     };
   },
@@ -130,7 +164,7 @@ export default defineComponent({
     this.getAllAttachedFiles();
   },
   methods: {
-    // getSelectedString() {
+    // getSelectedString() { 
     //   return this.selected.length === 0
     //     ? ""
     //     : `${this.selected.length}
@@ -143,23 +177,43 @@ export default defineComponent({
       this.pagination.rowsPerPage = e.pagination.rowsPerPage;
       // this.SendAttach();
     },
+    clkGoToAttachDetail(e, r) {
+      const opt = {
+        route:'/desktop/adm/getAttachFileDetail',
+        body: {
+          attachFileId: r._id
+        }
+      }
+      useFetch(opt).then((r) => {
+        if (r.error) {
+          this.$q.notify("Ocorreu um erro, tente novamente")
+          return
+        } else {
+          this.dialogAttachDetail.open = true
+          this.dialogAttachDetail.attachDetail = r.data
+        }
+      })
+    },
+    clearDialogDetail() {
+      this.dialogAttachDetail.attachDetail = null
+      this.dialogAttachDetail.open = false
+    },
     SendAttach() {
-      if (this.dialogAddAttach.title === '' || this.dialogAddAttach.description === '') {
+      if (this.dialogAddAttach.title === '' || this.dialogAddAttach.description === '' || this.dialogAddAttach.files === null) {
         this.$q.notify("Preencha todos os campos antes de enviar")
         return
       }
-      console.log(this.dialogAddAttach.files)
-      const file = [{file:this.dialogAddAttach.files,name:'userAttach'}]
+      const file = [{file:this.dialogAddAttach.files,name:'admAttach'}]
       const opt = {
         route: "/desktop/adm/addAttachFiles",
         body: {
           attachTitle: this.dialogAddAttach.title,
           attachDescription: this.dialogAddAttach.description
         },
-        files: null
+        file: null
       };
-      if(opt.files === null){
-        opt.files = file
+      if(this.dialogAddAttach.files !== null){
+        opt.file = file
       }
       useFetch(opt).then((r) => {
         if (r.error) {
@@ -179,7 +233,7 @@ export default defineComponent({
     },
     getAllAttachedFiles() {
       const opt = {
-        route: '/desktop/adm/getAttachedFilesByUserId',
+        route: '/desktop/adm/getAllAttachedFiles',
         body: {
           page: this.pagination.page,
           rowsPerPage: this.pagination.rowsPerPage
