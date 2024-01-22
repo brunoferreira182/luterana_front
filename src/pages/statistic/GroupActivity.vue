af5166f25e392e61dbfb18282c1cbe6027c43527
moraeszete <lucas.moraes24536@gmail.com>
2024-01-22 14:09:59 -0300

    Formulario de atividades em grupos

--- src/pages/statistic/GroupActivity.vue
+++ src/pages/statistic/GroupActivity.vue
@@ -1,21 +1,30 @@
 <template>
   <q-page-container class="no-padding">
     <q-page>
-      <div class="q-pt-md q-ma-md">{{ groupData.name }} Nome do grupo
-        <q-item class="card q-gutter-md">
-          <q-input :disabled="showInput" v-if="typeOfGroup === 'Jovens' || typeOfGroup === 'Juventude Mirim'" outlined
-            label="Padrinhos">
+      <q-list class="q-pa-sm">
+        <q-item v-for="item in groupData.childData" :key="item">
+          <q-item-section class=" item-section" clickable @click="toggleForm(item)">
+            <q-item-label > {{ item.organismName }} 
+              <q-btn
+                round
+                flat
+                dense
+                :icon="item.expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+                @click="item.expanded = !item.expanded"
+              />
+            </q-item-label>
+            
+            <q-slide-transition>
+              <div v-show="item.expanded">
+                <q-input label="Frequência total"></q-input>
+                <q-input label="Número de encontros"></q-input>
+                <q-btn type="submit" label="Salvar" @click="submitForm(item)"></q-btn>
+              </div>
+            </q-slide-transition>
+          </q-item-section>
 
-          </q-input>
-          <q-input outlined label="Frequência total">
-
-          </q-input>
-          <q-input outlined label="Número de encontros">
-
-          </q-input>
-        </q-item>
-      </div>
-      <q-separator></q-separator>
+          </q-item>
+      </q-list>
     </q-page>
   </q-page-container>
 </template>
@@ -27,14 +36,12 @@ export default defineComponent({
   name: "GroupActivity",
   data() {
     return {
-      showInput: false,
-      typeOfGroup: '',
+      expanded: false,
       groupData: {},
       statisticStatus: null
     }
   },
   beforeMount() {
-    this.getStatisticStatus()
     this.getCongregation()
   },
   methods: {
@@ -49,27 +56,25 @@ export default defineComponent({
       useFetch(opt).then((r) => {
         if (r.error) return
         this.groupData = r.data
-        //this.typeOfGroup = this.groupData.type
+        console.log('grupos',this.groupData)
       })
     },
-    getStatisticStatus() {
-      const opt = {
-        route: '/desktop/statistics/getStatisticStatusByOrganismId',
-        body: {
-          organismId: this.$route.query.organismId
-        }
+    toggleForm(item) {
+      item.formVisible = !item.formVisible
+      if (!item.formData) {
+        item.formData = {}
       }
-      useFetch(opt).then((r) => {
-        if (r.error) return
-        this.statisticStatus = r.data
-        this.verifyStatusTypes()
-      })
+    },
+    submitForm(item) {
+      // body:this.groupData.childData.organismConfigName
+      // Lógica para lidar com o envio do formulário
+      console.log("Formulário enviado para", item.organismConfigName);
+      // Você pode adicionar lógica adicional aqui, por exemplo, enviar dados para o servidor
     },
     goToStatistics() {
       const organismId = this.$route.query.organismId
       this.$router.push('/user/statistic?organismId=' + organismId)
     },
-    
   }
 })
 </script>
@@ -79,5 +84,15 @@ export default defineComponent({
   flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+.item-section {
+  position: relative;
+}
+.form-section {
+  position: absolute;
+  top: 100%; /* Ajuste conforme necessário para a posição desejada */
+  left: 0;
+  width: 100%;
+  z-index: 1;
 }
 </style>
