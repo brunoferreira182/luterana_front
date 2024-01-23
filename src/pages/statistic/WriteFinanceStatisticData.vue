97f837e587cad88ed3c080d63da4602a3aed8998
brunoferreira182 <brunoferreira1821995@gmail.com>
2024-01-23 08:53:19 -0300

    ajuste visual contribuiçlão writefinancestatisticdata

--- src/pages/statistic/WriteFinanceStatisticData.vue
+++ src/pages/statistic/WriteFinanceStatisticData.vue
@@ -9,150 +9,122 @@
             <q-breadcrumbs-el label="Financeiro" />
           </q-breadcrumbs>
         </div>
-        <div class="text-capitalize"> 
+        <div > 
           <div class="text-h5 q-mb-sm text-center">
             Estatística 2023
             <div class="text-h6">Dados Financeiros</div>
           </div>
           <div class="row">
             <div class="col q-gutter-y-md">
-              <div class="text-h5">
-                Entradas
-              </div>
-              <q-input 
-                outlined 
-                type="number"
-                prefix="R$"
-                label="Saldo anterior"
-                reverse-fill-mask
-                v-model.number="table.entries.saldoAnterior" 
-              />
-              <div class="no-margin">
-                Receitas regulares
+              <div style="border-radius: 1rem; background-color: rgb(245, 245, 245);" class="q-gutter-y-md q-pa-md">
+              
+                <div class="text-h5">
+                  Entradas
+                </div>
                 <q-input 
-                  outlined 
+                  type="number"
+                  prefix="R$"
+                  label="Saldo anterior"
+                  reverse-fill-mask
+                  v-model.number="table.entries.saldoAnterior" 
+                />
+                <div class="no-margin">
+                  <q-input 
+                    prefix="R$"
+                    type="number"
+                    label="Ofertas dominicais"
+                    reverse-fill-mask 
+                    v-model.number="table.entries.receitasRegulares.ofertasDominicais" 
+                  />
+                </div>
+                <div class="no-margin">
+                  <q-input 
+                    label="Ofertas mensais"
+                    prefix="R$"
+                    type="number"
+                    reverse-fill-mask 
+                    v-model.number="table.entries.receitasRegulares.ofertasMensais" 
+                  />
+                </div>
+                <div class="no-margin">
+                  <q-input 
+                    prefix="R$"
+                    type="number"
+                    label="Receitas de aluguéis"
+                    reverse-fill-mask 
+                    v-model.number="table.entries.receitasRegulares.receitasAlugueis" 
+                  />
+                </div>
+                <q-input 
+                  prefix="R$"
+                  label="Ofertas especiais"
+                  reverse-fill-mask 
+                  type="number"
+                  v-model.number="table.entries.ofertasEspeciais" 
+                />
+                <q-input 
+                  prefix="R$"
+                  label="Campanhas específicas"
+                  type="number"
+                  reverse-fill-mask 
+                  v-model.number="table.entries.campanhasEspecificas" 
+                />
+                <q-input 
+                  label="Auxílio"
+                  type="number"
+                  prefix="R$"
+                  reverse-fill-mask 
+                  v-model.number="table.entries.auxilio" 
+                />
+                <q-input 
+                  label="Empréstimos"
+                  prefix="R$"
+                  reverse-fill-mask 
+                  type="number"
+                  v-model.number="table.entries.emprestimos" 
+                />
+                <q-input 
+                  label="Todas as Outras receitas"
                   prefix="R$"
                   type="number"
-                  label="Ofertas dominicais"
                   reverse-fill-mask 
-                  v-model.number="table.entries.receitasRegulares.ofertasDominicais" 
+                  v-model.number="table.entries.todasOutrasReceitas" 
                 />
               </div>
-              <div class="no-margin">
-                Receitas regulares
+              <div style="border-radius: 1rem; background-color: rgb(245, 245, 245);" class="q-gutter-y-md q-pa-md">
+                <div class="text-h5">
+                  Saídas
+                </div>
+                <div class="text-h6">
+                  Contribuição registrada no SGA <q-chip color="blue text-white">R$ {{ contributionOutputSum ? contributionOutputSum : '0' }}</q-chip>
+                </div>
+                <div class="text-green" v-if="contributionCalculatedMore > 0">
+                  Contribuição registrada no SGA e calculado 11% R$ {{ contributionCalculatedMore }} <q-icon name="north"/>
+                </div>
+                <div class="text-red" v-else-if="contributionCalculatedLess > 0">
+                  Contribuição registrada no SGA e calculado 11% R$ {{ contributionCalculatedLess }} <q-icon name="south"/>
+                </div>
                 <q-input 
-                  outlined 
-                  label="Ofertas mensais"
+                  label="Contribuição Distrito"
+                  prefix="R$"
+                  type="number"
+                  v-model.number="table.output.contribuicaoDistrito" 
+                />
+                <q-input 
+                  label="Devolução de empréstimos IELB"
                   prefix="R$"
                   type="number"
                   reverse-fill-mask 
-                  v-model.number="table.entries.receitasRegulares.ofertasMensais" 
+                  v-model.number="table.output.devolucaoEmprestimoIELB" 
                 />
-              </div>
-              <div class="no-margin">
-                Receitas regulares
                 <q-input 
-                  outlined 
                   prefix="R$"
                   type="number"
-                  label="Receitas de aluguéis"
+                  label="Todas as outras saídas"
                   reverse-fill-mask 
-                  v-model.number="table.entries.receitasRegulares.receitasAlugueis" 
+                  v-model.number="table.output.todasSaidas" 
                 />
               </div>
-              <q-input 
-                outlined 
-                prefix="R$"
-                label="Ofertas especiais"
-                reverse-fill-mask 
-                type="number"
-                v-model.number="table.entries.ofertasEspeciais" 
-              />
-              <q-input 
-                outlined 
-                prefix="R$"
-                label="Campanhas específicas"
-                type="number"
-                reverse-fill-mask 
-                v-model.number="table.entries.campanhasEspecificas" 
-              />
-              <q-input 
-                outlined 
-                label="Auxílio"
-                type="number"
-                prefix="R$"
-                reverse-fill-mask 
-                v-model.number="table.entries.auxilio" 
-              />
-              <q-input 
-                outlined 
-                label="Empréstimos"
-                prefix="R$"
-                reverse-fill-mask 
-                type="number"
-                v-model.number="table.entries.emprestimos" 
-              />
-              <q-input 
-                outlined 
-                label="Todas as Outras receitas"
-                prefix="R$"
-                type="number"
-                reverse-fill-mask 
-                v-model.number="table.entries.todasOutrasReceitas" 
-              />
-              <div class="text-h5">
-                Saídas
-              </div>
-              <div class="text-h6">
-                Total de contribuições R$ {{ contributionOutputSum ? contributionOutputSum : '0' }}
-              </div>
-              <div class="text-green" v-if="contributionCalculatedMore > 0">
-                Total de contribuições calculado 11% R$ {{ contributionCalculatedMore }} <q-icon name="north"/>
-              </div>
-              <div class="text-red" v-else-if="contributionCalculatedLess > 0">
-                Total de contribuições calculado 11% R$ {{ contributionCalculatedLess }} <q-icon name="south"/>
-              </div>
-              <!-- <div class="no-margin">
-                Ofertas dominicais
-                {{ 
-                  table.output.contribuicaoIELB.ofertasDominicais ? table.output.contribuicaoIELB.ofertasDominicais : ''
-                }}
-              </div>
-              <div class="no-margin">
-                Ofertas mensais
-                {{ 
-                  table.output.contribuicaoIELB.ofertasMensais ? table.output.contribuicaoIELB.ofertasMensais : ''
-                }}
-              </div>
-              <div class="no-margin">
-                Receitas de alugueis 
-                {{ 
-                  table.output.contribuicaoIELB.receitasAlugueis ? table.output.contribuicaoIELB.receitasAlugueis : ''
-                }}
-              </div> -->
-              <q-input 
-                outlined 
-                label="Contribuição Distrito"
-                prefix="R$"
-                type="number"
-                v-model.number="table.output.contribuicaoDistrito" 
-              />
-              <q-input 
-                outlined 
-                label="Devolução de empréstimos IELB"
-                prefix="R$"
-                type="number"
-                reverse-fill-mask 
-                v-model.number="table.output.devolucaoEmprestimoIELB" 
-              />
-              <q-input 
-                outlined 
-                prefix="R$"
-                type="number"
-                label="Todas as outras saídas"
-                reverse-fill-mask 
-                v-model.number="table.output.todasSaidas" 
-              />
               <q-btn
                 label="Salvar como rascunho"
                 class="q-ma-md text-center"
@@ -263,7 +235,6 @@ export default defineComponent({
           return
         }
         this.$q.notify('Dados salvos como rascunho')
-        this.$router.back()
       });
     },
     getFinanceStatisticByOrganismId() {
