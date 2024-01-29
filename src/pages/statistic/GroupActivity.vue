ca074b78f11028bdec7b163b6b3d327edd5dcf33
moraeszete <lucas.moraes24536@gmail.com>
2024-01-29 11:11:55 -0300

    funcionando o atividades de grupos

--- src/pages/statistic/GroupActivity.vue
+++ src/pages/statistic/GroupActivity.vue
@@ -26,7 +26,7 @@
         </q-breadcrumbs>
       </div>
       <q-list>
-        <q-item v-for="item in departamentos" :key="item">
+        <q-item v-for="(item, i) in departamentos" :key="item">
           <div
             style="
               border-radius: 1rem;
@@ -57,12 +57,12 @@
                   >
                     <q-input
                       label="Frequência total"
-                      v-model="item.departamentoData.freqTotal"
+                      v-model="departamentos[i].departamentoData.freqTotal"
                     >
                     </q-input>
                     <q-input
                       label="Número de encontros"
-                      v-model="item.departamentoData.qtnEncontros"
+                      v-model="departamentos[i].departamentoData.qtnEncontros"
                     >
                     </q-input>
                   </q-item-section>
@@ -76,10 +76,10 @@
                     class="q-py-xs"
                   >
                     Há padrinhos:
-                    <q-radio v-model="item.departamentoData.padrinhos.exist" val="exist" label="Sim" />
-                    <q-radio v-model="item.departamentoData.padrinhos.exist" val="noExist" label="Não" />
+                    <q-radio v-model="departamentos[i].departamentoData.padrinhos.exist" val="exist" label="Sim" />
+                    <q-radio v-model="departamentos[i].departamentoData.padrinhos.exist" val="noExist" label="Não" />
                     <q-input
-                      v-model="item.departamentoData.padrinhos.qtn"
+                      v-model="departamentos[i].departamentoData.padrinhos.qtn"
                       v-if="padrinhos === 'exist'"
                       dense
                       class="q-mx-sm"
@@ -88,7 +88,7 @@
                   </q-item-label>
                   <!--  Sobre escolas dominicais ---->
                   <q-input
-                    v-model="item.departamentoData.professores"
+                    v-model="departamentos[i].departamentoData.professores"
                     v-if="
                       item.organismConfigName ===
                       'Departamento de Escola Dominical'
@@ -106,12 +106,12 @@
                       Aulas durante o culto</q-item-label
                     >
                     <q-input
-                      v-model="item.departamentoData.aulasCulto.freqCulto"
+                      v-model="departamentos[i].departamentoData.aulasCulto.freqCulto"
                       class="q-ml-md"
                       label="Frequência total durante culto"
                     ></q-input>
                     <q-input
-                      v-model="item.departamentoData.aulasCulto.qtnFaixaEtaria"
+                      v-model="departamentos[i].departamentoData.aulasCulto.qtnFaixaEtaria"
                       class="q-ml-md"
                       label="Divisão de faixa etária"
                     ></q-input>
@@ -127,13 +127,13 @@
                       Aulas no momento</q-item-label
                     >
                     <q-input
-                      v-model="item.departamentoData.aulasMomento.freqMomento"
+                      v-model="departamentos[i].departamentoData.aulasMomento.freqMomento"
                       class="q-ml-md"
                       label="Frequência total no momento"
                     ></q-input>
                     <q-input
                       v-model="
-                        item.departamentoData.aulasMomento.qtnFaixaEtaria
+                        departamentos[i].departamentoData.aulasMomento.qtnFaixaEtaria
                       "
                       class="q-ml-md"
                       label="Divisão de faixa etária"
@@ -148,12 +148,12 @@
                     <q-item-label>
                       Grupo Músical
                       <q-radio
-                        v-model="item.departamentoData.musicGroup.exist"
+                        v-model="departamentos[i].departamentoData.musicGroup.exist"
                         val="exist"
                         label="Sim"
                       />
                       <q-radio
-                        v-model="item.departamentoData.musicGroup.exist"
+                        v-model="departamentos[i].departamentoData.musicGroup.exist"
                         val="noExist"
                         label="Não"
                       />
@@ -162,7 +162,7 @@
                         v-if="
                           item.departamentoData.musicGroup.exist === 'banda'
                         "
-                        v-model="item.departamentoData.musicGroup.qtn"
+                        v-model="departamentos[i].departamentoData.musicGroup.qtn"
                         class="q-mx-xs q-mr-md"
                         label="Quantidade"
                       ></q-input>
@@ -170,19 +170,19 @@
                     <q-item-label>
                       Banda
                       <q-radio
-                        v-model="item.departamentoData.banda.exist"
+                        v-model="departamentos[i].departamentoData.banda.exist"
                         val="exist"
                         label="Sim"
                       />
                       <q-radio
-                        v-model="item.departamentoData.banda.exist"
+                        v-model="departamentos[i].departamentoData.banda.exist"
                         val="noExist"
                         label="Não"
                       />
                       <q-input
                         dense
                         v-if="item.departamentoData.banda.exist === 'exist'"
-                        v-model="item.departamentoData.banda.qtn"
+                        v-model="departamentos[i].departamentoData.banda.qtn"
                         class="q-mx-xs q-mr-md"
                         label="Quantidade"
                       ></q-input>
@@ -190,12 +190,12 @@
                     <q-item-label>
                       Grupo de Louvor
                       <q-radio
-                        v-model="item.departamentoData.grupoLouvor.exist"
+                        v-model="departamentos[i].departamentoData.grupoLouvor.exist"
                         val="exist"
                         label="Sim"
                       />
                       <q-radio
-                        v-model="item.departamentoData.grupoLouvor.exist"
+                        v-model="departamentos[i].departamentoData.grupoLouvor.exist"
                         val="noExist"
                         label="Não"
                       />
@@ -204,7 +204,7 @@
                         v-if="
                           item.departamentoData.grupoLouvor.exist === 'exist'
                         "
-                        v-model="item.departamentoData.grupoLouvor.qtn"
+                        v-model="departamentos[i].departamentoData.grupoLouvor.qtn"
                         class="q-mx-xs q-mr-md"
                         label="Quantidade"
                       ></q-input>
@@ -212,19 +212,19 @@
                     <q-item-label>
                       Quarteto
                       <q-radio
-                        v-model="item.departamentoData.quarteto.exist"
+                        v-model="departamentos[i].departamentoData.quarteto.exist"
                         val="exist"
                         label="Sim"
                       />
                       <q-radio
-                        v-model="item.departamentoData.quarteto.exist"
+                        v-model="departamentos[i].departamentoData.quarteto.exist"
                         val="noExist"
                         label="Não"
                       />
                       <q-input
                         dense
                         v-if="item.departamentoData.quarteto.exist === 'exist'"
-                        v-model="item.departamentoData.quarteto.qtn"
+                        v-model="departamentos[i].departamentoData.quarteto.qtn"
                         class="q-mx-xs q-mr-md"
                         label="Quantidade"
                       ></q-input>
@@ -232,19 +232,19 @@
                     <q-item-label>
                       Vocal
                       <q-radio
-                        v-model="item.departamentoData.vocal.exist"
+                        v-model="departamentos[i].departamentoData.vocal.exist"
                         val="exist"
                         label="Sim"
                       />
                       <q-radio
-                        v-model="item.departamentoData.vocal.exist"
+                        v-model="departamentos[i].departamentoData.vocal.exist"
                         val="noExist"
                         label="Não"
                       />
                       <q-input
                         dense
                         v-if="item.departamentoData.vocal.exist === 'exist'"
-                        v-model="item.departamentoData.vocal.qtn"
+                        v-model="departamentos[i].departamentoData.vocal.qtn"
                         class="q-mx-xs q-mr-md"
                         label="Quantidade"
                       ></q-input>
@@ -252,25 +252,25 @@
                     <q-item-section>
                       <q-input
                         class="q-mr-md"
-                        v-model="item.departamentoData.coro"
+                        v-model="departamentos[i].departamentoData.coro"
                         label="Total no Coro"
                       ></q-input>
                     </q-item-section>
                     <q-input
                       class="q-mr-md"
-                      v-model="item.departamentoData.musicosTotal"
+                      v-model="departamentos[i].departamentoData.musicosTotal"
                       label="Músicos envolvidos ao total"
                     ></q-input>
 
                     <q-item-section>
                       <q-input
                         class="q-mr-md"
-                        v-model="item.departamentoData.formalGroup.freqTotal"
+                        v-model="departamentos[i].departamentoData.formalGroup.freqTotal"
                         label="Frequência Total"
                       ></q-input>
                       <q-input
                         class="q-mr-md"
-                        v-model="item.departamentoData.formalGroup.qtnEncontros"
+                        v-model="departamentos[i].departamentoData.formalGroup.qtnEncontros"
                         label="Número de encontros"
                       ></q-input>
                     </q-item-section>
@@ -282,44 +282,44 @@
                     "
                   >
                     <q-input
-                      v-model="item.departamentoData.visitadoresTotal"
+                      v-model="departamentos[i].departamentoData.visitadoresTotal"
                       class="q-pl-sm q-mr-md"
                       label="Total de visitantes do grupo"
                     />
                     <q-item-label class="q-mt-sm" /> Visitas Missionárias
                     <q-input
                       v-model="
-                        item.departamentoData.visitasMissionarias.qtnVisitas
+                        departamentos[i].departamentoData.visitasMissionarias.qtnVisitas
                       "
                       class="q-pl-sm q-mr-md"
                       label="Número de visitas"
                     />
                     <q-input
                       v-model="
-                        item.departamentoData.visitasMissionarias.pessoas
+                        departamentos[i].departamentoData.visitasMissionarias.pessoas
                       "
                       class="q-pl-sm q-mr-md"
                       label="Total de pessoas"
                     />
                     <q-item-label class="q-mt-sm" /> Visitas à Membros
                     <q-input
-                      v-model="item.departamentoData.visitasMembros.qtnVisitas"
+                      v-model="departamentos[i].departamentoData.visitasMembros.qtnVisitas"
                       class="q-pl-sm q-mr-md"
                       label="Número de visitas"
                     />
                     <q-input
-                      v-model="item.departamentoData.visitasMembros.pessoas"
+                      v-model="departamentos[i].departamentoData.visitasMembros.pessoas"
                       class="q-pl-sm q-mr-md"
                       label="Total de pessoas"
                     />
                     <q-item-label class="q-mt-sm" /> Enfermos
                     <q-input
-                      v-model="item.departamentoData.visitasEnfermos.qtnVisitas"
+                      v-model="departamentos[i].departamentoData.visitasEnfermos.qtnVisitas"
                       class="q-pl-sm q-mr-md"
                       label="Número de visitas"
                     />
                     <q-input
-                      v-model="item.departamentoData.visitasEnfermos.enfermos"
+                      v-model="departamentos[i].departamentoData.visitasEnfermos.enfermos"
                       class="q-pl-sm q-mr-md"
                       label="Total de enfermos"
                     />
@@ -331,22 +331,22 @@
                     "
                   >
                     <q-input
-                      v-model="item.departamentoData.pessoasEnvol"
+                      v-model="departamentos[i].departamentoData.pessoasEnvol"
                       class="q-pl-sm q-mr-md"
                       label="Total de pessoas envolvidas"
                     />
                     <q-input
-                      v-model="item.departamentoData.pessoasBeneft"
+                      v-model="departamentos[i].departamentoData.pessoasBeneft"
                       class="q-pl-sm q-mr-md"
                       label="Total de pessoas Beneficiadas"
                     />
                     <q-input
-                      v-model="item.departamentoData.permProgs"
+                      v-model="departamentos[i].departamentoData.permProgs"
                       class="q-pl-sm q-mr-md"
                       label="Descrição dos programas permanentes"
                     />
                     <q-input
-                      v-model="item.departamentoData.acoesEventuais"
+                      v-model="departamentos[i].departamentoData.acoesEventuais"
                       class="q-pl-sm q-mr-md"
                       label="Ações eventuais"
                     />
@@ -354,22 +354,22 @@
                   <!-- Sobre outros   -->
                   <q-item-section v-if="item.organismConfigName === 'Outros'">
                     <q-input
-                      v-model="item.departamentoData.finalidade"
+                      v-model="departamentos[i].departamentoData.finalidade"
                       class="q-pl-sm q-mr-md"
                       label="Finalidade do grupo"
                     ></q-input>
                     <q-input
-                      v-model="item.departamentoData.organizacao"
+                      v-model="departamentos[i].departamentoData.organizacao"
                       class="q-pl-sm q-mr-md"
                       label="Organização do grupo"
                     ></q-input>
                     <q-input
-                      v-model="item.departamentoData.freqTotal"
+                      v-model="departamentos[i].departamentoData.freqTotal"
                       class="q-pl-sm q-mr-md"
                       label="Número de encontros"
                     ></q-input>
                     <q-input
-                      v-model="item.departamentoData.qtnEncontros"
+                      v-model="departamentos[i].departamentoData.qtnEncontros"
                       class="q-pl-sm q-mr-md"
                       label="Frequência total "
                     ></q-input>
@@ -393,7 +393,8 @@ export default defineComponent({
   data() {
     return {
       departamentos: [],
-      formDepart:[],
+      rascunhos: [],
+      formDepart: [],
       congregationName: "",
       statisticStatus: null,
       arrayIgnore: [
@@ -407,10 +408,10 @@ export default defineComponent({
   },
   beforeUnmount() {
     this.submitAllItens();
-    const timer = setTimeout(() => {
-      console.log("Timer executado ao desmontar a página!");
-      clearTimeout(timer);
-    }, 5000);
+    // const timer = setTimeout(() => {
+    //   console.log("Timer executado ao desmontar a página!");
+    //   clearTimeout(timer);
+    // }, 5000);
   },
   beforeMount() {
     this.getCongregationGroups();
@@ -435,9 +436,10 @@ export default defineComponent({
       useFetch(opt).then((r) => {
         if (r.error) return;
         this.departamentos = r.data.childData;
+        console.log(this.departamentos,"cuzinho doce");
         this.congregationName = r.data.organismName;
-        this.getSketchGroup();
       });
+      this.getSketchGroup();
     },
     getSketchGroup() {
       const opt = {
@@ -447,23 +449,20 @@ export default defineComponent({
         }
       };
       useFetch(opt).then((r) => {
-        console.log("Rascunho pego do estatisca:", r.data[0]);
+        console.log("Rascunho pego do estatisca:", r.data);
         if (r.error) return;
-
-        this.departamentos.forEach((departamento) => {
-          const sketch = r.data.departamentoData.find(
-            (sketch) =>
-              sketch.organismConfigName ===
-              departamento.departamentoData.organismConfigName
-          );
-          console.log(sketch, 'dbnasidbhaujhbsdjhabsjdbasjb');
-          if (sketch) {
-            departamento.departamentoData = sketch.departamentoData;
-            console.log("departamento updated", departamento);
+        this.rascunhos = r.data[0]
+        console.log(this.rascunhos, 'dlkmaskd')
+        
+        this.departamentos.forEach((item) =>{
+          const rask = this.rascunhos.departamentoData.find((rascunho) => 
+            rascunho.organismConfigName === item.organismConfigName
+          )
+          if (rask){
+            item.departamentoData = rask
+            console.log('atualizado', item);
           }
-          
-        });
-        console.log(this.departamentos, "rascunho interado por departamento");
+        })
       });
     },
     expandItem(item) {
