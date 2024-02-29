9f7e70aad83fb2721eb0a3a52c135a521bd50cea
brunoferreira182 <brunoferreira1821995@gmail.com>
2024-02-29 16:17:48 -0300

    ajuste organismdetail para separar atuacao e chamado

--- src/components/CardPastor.vue
+++ src/components/CardPastor.vue
@@ -1,65 +1,103 @@
 <template>
-  {{ teste }}
   <q-card
     style="border-radius: 1rem"
     class="bg-white q-ma-sm q-pa-sm"
     flat
   >
-    <q-item
+    Chamados
+    <div
       v-for="(user) in props.func.users"
       :key="user"
-      style="border-radius: 0.5rem;"
-      class="bg-grey-3 q-ma-xs"
+
     >
-    
-      <q-item-section avatar>
-        <div class="text-capitalize" >
-          <div v-if="user.functionSubtype === 'atuacao'">
-            Atuação
-          </div>
-          <div v-if="user.functionSubtype === 'chamado'">
-            Chamado
-          </div>
-        </div>
-        <q-img 
-          style="border-radius: 1rem"
-          :src="user.userImage ? utils.makeFileUrl(user.userImage) : avatar" 
-          width="46px" 
-          height="46px"
-        />
-      </q-item-section>
-      <q-item-section class="text-wrap" lines="2" v-if="user.userName">
-        {{ user.userName }}
-      </q-item-section>
-      <q-item-section side>
-        <q-item-label>
-          <!-- <q-btn
-            v-if="canEditPastor"
-            icon="sync"
-            flat
-            dense
-            rounded
-            color="primary"
-            @click="swapPastorToFunctionPastor(user)"
-            :disable="disableButtons"
-          >
-            <q-tooltip>Trocar pastor</q-tooltip>
-          </q-btn> -->
-          <q-btn
-            v-if="canEditPastor && user.functionSubtype === 'chamado'"
-            icon="delete"
-            flat
-            dense
-            rounded
-            color="red"
-            @click="deleteUserFromFunction(user)"
-            :disable="disableButtons"
-          >
-            <q-tooltip>Remover pastor</q-tooltip>
-          </q-btn>
-        </q-item-label>
-      </q-item-section>
-    </q-item>
+      <q-item
+        v-if="user.functionSubtype === 'chamado'"
+        style="border-radius: 0.5rem;"
+        class="bg-grey-3 q-ma-xs"
+      > 
+      
+        <q-item-section avatar>
+          <q-img 
+            style="border-radius: 1rem"
+            :src="user.userImage ? utils.makeFileUrl(user.userImage) : avatar" 
+            width="46px" 
+            height="46px"
+          />
+        </q-item-section>
+        <q-item-section class="text-wrap" lines="2" v-if="user.userName">
+          {{ user.userName }}
+        </q-item-section>
+        <q-item-section side>
+          <q-item-label>
+            <!-- <q-btn
+              v-if="canEditPastor"
+              icon="sync"
+              flat
+              dense
+              rounded
+              color="primary"
+              @click="swapPastorToFunctionPastor(user)"
+              :disable="disableButtons"
+            >
+              <q-tooltip>Trocar pastor</q-tooltip>
+            </q-btn> -->
+            <q-btn
+              v-if="canEditPastor"
+              icon="delete"
+              flat
+              dense
+              rounded
+              color="red"
+              @click="deleteUserFromFunction(user)"
+              :disable="disableButtons"
+            >
+              <q-tooltip>Remover pastor</q-tooltip>
+            </q-btn>
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </div>
+      Atuações
+    <div
+      v-for="(user) in props.func.users"
+      :key="user"
+
+    >
+      <q-item
+        v-if="user.functionSubtype === 'atuacao'"
+        style="border-radius: 0.5rem;"
+        class="bg-grey-3 q-ma-xs"
+      > 
+      
+        <q-item-section avatar>
+          <q-img 
+            style="border-radius: 1rem"
+            :src="user.userImage ? utils.makeFileUrl(user.userImage) : avatar" 
+            width="46px" 
+            height="46px"
+          />
+        </q-item-section>
+        <q-item-section class="text-wrap" lines="2" v-if="user.userName">
+          {{ user.userName }}
+        </q-item-section>
+        <q-item-section side>
+          <q-item-label>
+            <!-- <q-btn
+              v-if="canEditPastor"
+              icon="sync"
+              flat
+              dense
+              rounded
+              color="primary"
+              @click="swapPastorToFunctionPastor(user)"
+              :disable="disableButtons"
+            >
+              <q-tooltip>Trocar pastor</q-tooltip>
+            </q-btn> -->
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </div>
   </q-card>
 </template>
 
@@ -67,7 +105,16 @@
 // import { defineComponent } from "vue";
 import avatar from '../assets/avatar.svg'
 import utils from '../boot/utils'
-const props = defineProps(['func', 'funcIndex', 'showAddUserButton', 'showInviteUserButton', 'isMobile', 'photo', 'canEditPastor', 'disableButtons'])
+const props = defineProps([
+'func', 
+'funcIndex', 
+'showAddUserButton',
+'showInviteUserButton', 
+'isMobile', 
+'photo', 
+'canEditPastor', 
+'disableButtons', 
+])
 const emits = defineEmits(['remove', 'deleteUserFromFunction'])
 
 
