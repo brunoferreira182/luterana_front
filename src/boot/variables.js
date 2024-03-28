3e55b47e83296d5d81af5842aebde1982093e0d9
Leonardo <leorocha2004@gmail.com>
2024-03-28 09:29:36 -0300

    ajustes para buildar - correção para usuário poder editar suas congregações

--- src/boot/variables.js
+++ src/boot/variables.js
@@ -1,8 +1,8 @@
 //////////// VARIAVEIS DA APLICACAO //////
-const MODE_MASTER_SERVER = "local";
+const MODE_MASTER_SERVER = "prod";
 const COMPANY_ID = 10;
-const VERSION = "1.10 Beta"
-const BUILD = 31
+const VERSION = "1.11 Beta"
+const BUILD = 32
 // NAO ALTERAR //
 const MODE_IFRAME = MODE_MASTER_SERVER;
 const MODE_AUTH_SERVER = "local";
