df64ec412ae952458ae6e2748e6a2c545fc0e5a4
Leonardo <leorocha2004@gmail.com>
2024-04-04 09:31:26 -0300

    ajustes para buildar

--- src/boot/variables.js
+++ src/boot/variables.js
@@ -1,8 +1,8 @@
 //////////// VARIAVEIS DA APLICACAO //////
-const MODE_MASTER_SERVER = "local";
+const MODE_MASTER_SERVER = "prod";
 const COMPANY_ID = 10;
-const VERSION = "1.14 Beta"
-const BUILD = 35
+const VERSION = "1.15 Beta"
+const BUILD = 36
 // NAO ALTERAR //
 const MODE_IFRAME = MODE_MASTER_SERVER;
 const MODE_AUTH_SERVER = "local";
